f12cdffd59150ae35048ed7cdec9985b0482237e
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-13 13:57:42 -0300

    Agregue algunas modificaciones en los componentes

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -5,5 +5,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+/*
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App.js";
 
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+ 
+  document.getElementById("root")
+);
+*/
 serviceWorker.unregister();
