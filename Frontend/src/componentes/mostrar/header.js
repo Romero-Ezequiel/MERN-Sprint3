c3b48a00114a9a96d2ba6d6e2be9436b39f3e8a6
Romero-Ezequiel <romerorezequiel@gmail.com>
2019-12-16 02:08:05 -0300

    Hice algunos cambios en el componente de la cabecera

--- Frontend/src/componentes/mostrar/header.js
+++ Frontend/src/componentes/mostrar/header.js
@@ -4,13 +4,17 @@ import React from 'react';
 //Importo la imagen de MYtinerary para embeberla en la cabecera 
 import MYtinerary from '../img/MYtinerary.png';
 import '../estilos/estilo.css';
+import Nav from './Nav';
 
-
-class Header extends React.Component{
+class Header extends React.Component {
     render() {
-        return(
+        return (
             //Utilizo JSX para armar la interfaz de usuario del componente header
-            <img src={MYtinerary} alt='Imagen header' className="App-title"  />
+
+            <div>
+                <Nav />
+                <img src={MYtinerary} alt='Imagen header' className="App-title" />
+            </div>
         );
     }
 }
